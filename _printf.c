d9f81ea46cf70274742396758d228f3aaeeed79f
Iammarv0h <marvinaliero161@gmail.com>
2023-07-25 21:16:24 +0300

    printf

--- _printf.c
+++ _printf.c
@@ -1,14 +1,11 @@
 #include "main.h"
 
-/* Function prototypes */
 void print_buffer(char buffer[], int *buff_ind);
-void print_humorous_message(void); /* New function for %r format */
 
 /**
- * _printf - Custom printf function
+ * _printf - Printf function
  * @format: The format string.
- *
- * Return: The number of characters printed.
+ * Return: Number of printed characters.
  */
 int _printf(const char *format, ...)
 {
@@ -22,15 +19,13 @@ int _printf(const char *format, ...)
 
 	va_start(list, format);
 
-	for (i = 0; format[i] != '\0'; i++)
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
 		{
 			buffer[buff_ind++] = format[i];
 			if (buff_ind == BUFF_SIZE)
-			{
 				print_buffer(buffer, &buff_ind);
-			}
 			printed_chars++;
 		}
 		else
@@ -41,21 +36,11 @@ int _printf(const char *format, ...)
 			precision = get_precision(format, &i, list);
 			size = get_size(format, &i);
 			++i;
-			if (format[i] == 'r') /* Check for %r format */
-			{
-				print_humorous_message(); /* Call the function to print humorous message */
-			}
-			else
-			{
-				printed = handle_print(format, &i, list, buffer,
-					flags, width, precision, size);
-				if (printed == -1)
-				{
-					va_end(list);
-					return (-1);
-				}
-				printed_chars += printed;
-			}
+			printed = handle_print(format, &i, list, buffer,
+					       flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
 		}
 	}
 
@@ -67,28 +52,15 @@ int _printf(const char *format, ...)
 }
 
 /**
- * print_buffer - Prints the contents of the buffer if it's not empty
- * @buffer: The character buffer.
- * @buff_ind: Pointer to the buffer index.
+ * print_buffer - Prints the contents of the buffer if it exists
+ * @buffer: Array of characters.
+ * @buff_ind: Index at which to add next character, represents the length.
  */
 void print_buffer(char buffer[], int *buff_ind)
 {
 	if (*buff_ind > 0)
-	{
 		write(1, &buffer[0], *buff_ind);
-	}
 
 	*buff_ind = 0;
-}
-
-/**
- * print_humorous_message - Prints a humorous message for %r format specifier.
- * You can customize the message as you like.
- */
-void print_humorous_message(void)
-{
-	char message[] = "Whoa! You found a mysterious format specifier (%r). "
-			"I'm just a simple printf function, not a magician! 😄";
-	write(1, message, sizeof(message) - 1);
 }
 
